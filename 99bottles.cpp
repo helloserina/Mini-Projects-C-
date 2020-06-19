fc26989dde33c135e9a520866af68504d3c65ed9
helloserina <57045574+helloserina@users.noreply.github.com>
2020-06-19 12:00:51 -0700

    Create 99bottles.cpp

---
+++ 99bottles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+  // Write a for loop here:
+  for (int i = 99; i > 0; i--) {
+    std::cout << i << " bottles of pop on the wall.\nTake one down and pass it around.\n" << i-1 << " bottles of pop on the wall.";
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
